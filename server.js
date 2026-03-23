c1fa652f8e3214f8fd6524a6145cc02dd83592ae
tomaintainmyself2022-star <tomaintainmyself2022@gmail.com>
2026-03-23 16:58:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,28 +9,48 @@ app.use(express.static("public"));
 
 // MongoDB Connection
 mongoose.connect("mongodb+srv://tomaintainmyself2022_db_user:UKR56EuHkUk6Xi4R@cluster0.gipthnd.mongodb.net/studentDB?retryWrites=true&w=majority")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+.then(() => {
+    console.log("MongoDB Connected");
+
+    // START SERVER ONLY AFTER DB CONNECTS
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Server running");
+    });
+})
+.catch(err => {
+    console.error("MongoDB Error:", err);
+});
 
 // Model
 const Student = require("./models/Student");
 
 // Add Student
 app.post("/add", async (req, res) => {
-    const { name, age } = req.body;
-    const student = new Student({ name, age });
-    await student.save();
-    res.send("Student Added");
+    try {
+        const { name, age } = req.body;
+
+        if (!name || !age) {
+            return res.status(400).json({ error: "Missing data" });
+        }
+
+        const student = new Student({ name, age });
+        await student.save();
+
+        res.json({ message: "Student Added" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
 // Get Students
 app.get("/students", async (req, res) => {
-    const students = await Student.find();
-    res.json(students);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log("Server running");
+    try {
+        const students = await Student.find();
+        res.json(students);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
